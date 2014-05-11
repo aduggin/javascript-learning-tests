2580e0ca1e02350e08465f8442a5ab0225337bed
Alistair Duggin <alistair.duggin@btinternet.com>
2014-05-11 19:15:50 +0100

    Set up Karma and add gitignore

---
+++ karma.conf.js
@@ -0,0 +1,18 @@
+module.exports = function(config) {
+    config.set({
+        basePath: 'app',
+        frameworks: ['mocha-debug', 'mocha', 'chai', 'sinon'],
+        files: [
+            'tests/**/*.js'
+        ],
+        exclude: [],
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_ERROR,
+        autoWatch: false,
+        browsers: ['Safari', 'Chrome', 'PhantomJS'],
+        captureTimeout: 6000,
+        singleRun: false
+    });
+};
